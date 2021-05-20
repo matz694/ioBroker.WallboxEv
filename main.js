99d56566372da8e1c1c3d99c84000f49820761b9
Matthias Stroske <matthias@Matthiass-MBP.fritz.box>
2021-05-20 20:31:35 +0200

    minor additions

--- main.js
+++ main.js
@@ -36,8 +36,8 @@ class WallboxEv extends utils.Adapter {
 
 		// The adapters config (in the instance object everything under the attribute "native") is accessible via
 		// this.config:
-		this.log.info("config option1: " + this.config.option1);
-		this.log.info("config option2: " + this.config.option2);
+		this.log.info("config User: " + this.config.user);
+		this.log.info("config Password: " + this.config.password);
 
 		/*
 		For every state in the system there has to be also an object of type state
